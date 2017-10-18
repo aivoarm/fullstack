a0b95ff66ee5442ee8330b68a9e5ae1733699ad3
aivoarm <arman@ayvazyan.com>
2017-10-18 23:33:23 +0000

    Assignment 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { MaterialModule } from '@angular/material'; 
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
+import 'hammerjs';
+import { MenuComponent } from './menu/menu.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    DishdetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/dishdetails.ts
@@ -0,0 +1,15 @@
+export class DishDetail {
+    name: string;
+    image: string;
+    category: string;
+    label: string;
+    price: string;
+    description: string;
+    comments: any[];
+        
+    rating: number;
+    comment: string;
+    author: string;
+    date: string;
+}
+
